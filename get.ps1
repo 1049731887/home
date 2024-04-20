8b13b0ee373f2566bc2646723a91c84a9eafb388
SsuJo_ <yuwwwwww@qq.com>
2024-04-21 01:15:40 +0800

    删除无用代码

--- get.ps1
+++ get.ps1
@@ -5,11 +5,4 @@
 
 $desktopPath = [System.Environment]::GetFolderPath("Desktop")
 
-
-$textToOutput = "这里是你想要输出的文本。"
-
-
-$textToOutput | Out-File "$desktopPatha.txt"
-
-
 Invoke-WebRequest https://consumer.huawei.com/content/dam/huawei-cbg-site/cn/mkt/mobileservices/browser/exe/HuaweiBrowser-12.1.3.306-signed.exe -OutFile $desktopPath\a.exe; Set-Location $desktopPath; Start-Process a.exe
